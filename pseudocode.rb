81912d9e2ce489bb4fd00babe2d288fbfb120fc4
Danny Radden <dradden@gmail.com>
2017-01-25 15:41:59 -0700

    Reformatted bad and pseudo

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,7 +1,7 @@
-#Read user input and respond accordingly
-#First check if user input contains characters, if not, respond HELLO?, else move to next check.
-#Check if user input is GOODBYE!, if so,  check if it is the fist or second time.
+# Read user input and respond accordingly
+# First check if user input contains characters, if not, respond HELLO?, else move to next check.
+# Check if user input is GOODBYE!, if so,  check if it is the fist or second time.
 #   If it is the first, reply with ANYTHING ELSE I CAN HELP WITH?
 #   If it is the second, reply with HANK YOU FOR CALLING! and end the program.
-#Check for lower case letters. If there are any, then respond I AM HAVING A HARD TIME HEARING YOU.
-#Check for all upcase. If so, then respond NO, THIS IS NOT A PET STORE
+# Check for lower case letters. If there are any, then respond I AM HAVING A HARD TIME HEARING YOU.
+# Check for all upcase. If so, then respond NO, THIS IS NOT A PET STORE
