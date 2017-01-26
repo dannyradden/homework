86db5e489c63747e4d0800646dd0b1896b85bd5a
Danny Radden <dradden@gmail.com>
2017-01-26 09:39:31 -0700

    super_fizz updated

--- super_fizz.rb
+++ super_fizz.rb
@@ -6,3 +6,27 @@
 
   puts message == "" ? i : message
 end
+
+# Alternate method:
+#
+# i = -1
+#  while i <= 999
+#    i += 1
+#    if (i % 3 == 0) && (i % 5 == 0) && (i % 7 == 0)
+#      puts "SuperFizzBuzz"
+#    elsif (i % 3 == 0) && (i % 7 == 0)
+#      puts "SuperFizz"
+#    elsif (i % 5 == 0) && (i % 7 == 0)
+#      puts "Superbuzz"
+#    elsif (i % 3 == 0) && (i % 5 == 0)
+#      puts "FizzBuzz"
+#    elsif (i % 3 == 0)
+#      puts "Fizz"
+#    elsif (i % 5 == 0)
+#      puts "Buzz"
+#    elsif (i % 7 == 0)
+#      puts "Super"
+#    else
+#      puts i
+#    end
+#  end
