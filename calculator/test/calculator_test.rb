04431669ef513ab3ff6ce11ddb1e6233dd9475b2
Danny Radden <dradden@gmail.com>
2017-02-02 09:47:35 -0700

    ...

--- calculator/test/calculator_test.rb
+++ calculator/test/calculator_test.rb
@@ -11,16 +11,19 @@ class CalculatorTesting < Minitest::Test
 
 
   def test_total
-    assert_equal 20, calc.total(3,4,6,2,5)
+    assert_equal 20, calc.total(3, 4, 6, 2, 5)
   end
 
   def test_add
-    assert_equal 4, calc.add([1,3])
+    assert_equal 4, calc.add([1, 3])
   end
 
   def test_subtract
+    assert_equal 5, calc.subtract([8, 3])
   end
 
   def test_clear
+    calc.clear
+    assert number_array == []
   end
 end
